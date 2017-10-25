58de16e878e8c8215cae2350f17586d7da15a236
Juan Manuel Contreras <jm-contreras@users.noreply.github.com>
2017-10-24 21:46:55 -0400

    Add WMATA edge list and Python file to analyze contents.

--- analyze_example_edgelist.py
+++ analyze_example_edgelist.py
@@ -4,5 +4,5 @@ import pandas as pd
 # Load data
 df = pd.read_csv('~/git/postman_problems/postman_problems/examples/seven_bridges/edgelist_seven_bridges.csv')
 
-# Show example edge list
-df.head()
+# Show entire edge list
+print(df)
---
+++ build_edgelist.py
@@ -0,0 +1,8 @@
+# Import module
+import pandas as pd
+
+# Load edge list
+df = pd.read_csv('edgelist_wmata.csv')
+
+# Show entire edge list
+print(df)
