94324143ed2443e4a8ec1832e951001ffbda91a2
Juan Manuel Contreras <jm-contreras@users.noreply.github.com>
2017-10-24 21:02:42 -0400

    Add analysis of example edgelist.

---
+++ analyze_example_edgelist.py
@@ -0,0 +1,8 @@
+# Import module
+import pandas as pd
+
+# Load data
+df = pd.read_csv('~/git/postman_problems/postman_problems/examples/seven_bridges/edgelist_seven_bridges.csv')
+
+# Show example edge list
+df.head()
