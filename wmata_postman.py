1984fd804cae97fc9354bc9b3e01f61a3c5a97d3
Juan Manuel Contreras <jm-contreras@users.noreply.github.com>
2017-11-04 18:01:58 -0400

    Compute shortest distance between each pair of nodes in graph.

--- wmata_postman.py
+++ wmata_postman.py
@@ -3,6 +3,7 @@ import itertools as it
 import networkx as nx
 import pandas as pd
 
+
 # Create empty graph
 graph = nx.Graph()
 
@@ -24,3 +25,7 @@ nodes_odd_degree = [v for v, d in graph.degree if d % 2 == 1]
 # Compute odd node pairs
 odd_node_pairs = list(it.combinations(nodes_odd_degree, 2))
 
+# Compute shortest distance between each pair of nodes in graph
+distances = {}
+for pair in odd_node_pairs:
+    distances[pair] = nx.dijkstra_path_length(graph, pair[0], pair[1], weight='distance')
